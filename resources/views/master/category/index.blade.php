04974ca78649aeeba665baafa999e138bf0d353d
Tran Thanh Thuy <52300280@student.tdtu.edu.vn>
2026-07-03 14:49:57 +0700

    UPDATE: Giới hạn ký tự mã category

--- resources/views/master/category/index.blade.php
+++ resources/views/master/category/index.blade.php
@@ -334,7 +334,7 @@
 
   document.getElementById('catCode').addEventListener('input', function () {
     const pos = this.selectionStart;
-    this.value = this.value.toUpperCase();
+    this.value = this.value.toUpperCase().replace(/[^A-Z0-9]/g, ''); // UPDATE
     this.setSelectionRange(pos, pos);
   });
 
